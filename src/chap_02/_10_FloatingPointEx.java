4031b7c62477e8bdb35d72d1482f8e286319fe32
prao <pch5521@gmail.com>
2023-01-27 23:32:48 +0900

    chapter 2. end of casting

--- src/chap_02/_10_FloatingPointEx.java
+++ src/chap_02/_10_FloatingPointEx.java
@@ -1,6 +1,6 @@
 package chap_02;
 
-public class FloatingPointEx {
+public class _10_FloatingPointEx {
     public static void main(String[] args) {
         double num = 0.1;
         for(int i = 0; i < 1000; i++) {
