618c0677facd1981f8587d0dbae6b0e2c169682a
prao <pch5521@gmail.com>
2023-03-01 20:17:21 +0900

    복습중

--- src/chap_06/_23_ConstructorTest.java
+++ src/chap_06/_23_ConstructorTest.java
@@ -15,6 +15,6 @@ class Data2 {
 public class _23_ConstructorTest {
     public static void main(String[] args) {
         Data1 d1 = new Data1();
-        Data2 d2 = new Data2();   // 컴파일 에러 발생
+//        Data2 d2 = new Data2();   // 컴파일 에러 발생
     }
 }
