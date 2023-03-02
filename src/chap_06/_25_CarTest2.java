8b2434188a79bf99f0dce28dc8e0c2b5ffa0a140
prao <pch5521@gmail.com>
2023-03-02 23:44:26 +0900

    OOP 끝나간다 ,,

---
+++ src/chap_06/_25_CarTest2.java
@@ -0,0 +1,28 @@
+package chap_06;
+
+class Car2 {
+    String color;
+    String gearType;
+    int    door;
+
+    Car2() {
+        this("white", "auto", 4);
+    }
+
+    Car2(String color) {
+        this(color, "auto", 4);
+    }
+
+    Car2(String color, String gearType, int door) {
+        this.color    = color;
+        this.gearType = gearType;
+        this.door     = door;
+    }
+}
+
+public class _25_CarTest2 {
+    public static void main(String[] args) {
+        Car2 c1 = new Car2();
+        Car2 c2 = new Car2("blue");
+    }
+}
