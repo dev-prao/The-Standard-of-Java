a45aee9f9d87e6daa7e3506b1e957c35fc7fa170
prao <pch5521@gmail.com>
2023-05-18 16:16:14 +0900

    다시 예외처리 공부중

---
+++ src/chap_08/_01_ExceptionEx1.java
@@ -0,0 +1,21 @@
+package chap_08;
+
+public class _01_ExceptionEx1 {
+    public static void main(String[] args) {
+        try {
+//            try {
+//            } catch (Exception e) {
+//            }
+        } catch (Exception e) {
+//            try {
+//            } catch (Exception e) {
+//            }   // 에러. 변수 e가 중복 선언되었다.
+        } // try-catch의 끝
+
+        try {
+
+        } catch (Exception e) {
+
+        } // try-catch의 끝
+    } // main 메서드의 끝
+}
